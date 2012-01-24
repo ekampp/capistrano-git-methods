fb032acb636df4f61167189993e38ebbe8890892
Emil Kampp <emiltk@benjamin.dk>
2012-01-24 09:11:53 +0100

    Removed some methods not specifically for the git scope.

--- lib/capistrano-git-methods.rb
+++ lib/capistrano-git-methods.rb
@@ -1,29 +1,43 @@
 require "capistrano-git-methods/version"
+
+#
+# Extends the capistrano configuration, to allow the <tt>git</tt> namespace to
+# be available within the capistrano recipe.
+#
+# == Requirements
+#
+# This requires the <tt>current_path</tt> variable to be present. This should
+# already be available in the standard capistrano deploy scheme.
+#
 Capistrano::Configuration.instance.load do
   namespace :git do
+
+    #
+    # Clones the repository onto the remote server.
+    #
+    # This is required before you can run the <tt>update_code</tt> method
+    # successfully.
+    #
     desc "Setup your git-based deployment app"
     task :setup, :except => { :no_release => true } do
       dirs = [deploy_to, shared_path]
       dirs += shared_children.map { |d| File.join(shared_path, d) }
-      run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+      run "mkdir -p #{dirs.join(' ')} && chmod g+w #{dirs.join(' ')}"
       run "git clone #{repository} #{current_path}"
     end
 
+    #
+    # Updates the remote code to the latest head of the selected
+    # git branch.
+    #
     desc "Update the deployed code."
     task :update_code, :except => { :no_release => true } do
       run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
-      finalize_update
-    end
-
-    desc "Installs the bundle gems"
-    task :bundle, :except => { :no_release => true } do
-      run "cd #{current_path}; bundle --deployment --without test --without development"
     end
 
     desc "Moves the repo back to the previous version of HEAD"
     task :repo, :except => { :no_release => true } do
       set :branch, "HEAD@{1}"
-      deploy.default
     end
 
     desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
