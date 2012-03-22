f02d5c323a8c76f12ace4948759879f5471d22f0
Emil Kampp <emiltk@benjamin.dk>
2012-03-22 05:12:48 +0100

    Quiet git checkouts

--- capistrano-git-methods.gemspec
+++ capistrano-git-methods.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "rake"
   s.add_development_dependency "yard"
   s.add_runtime_dependency "git"
+  s.add_runtime_dependency "shell-spinner"
 end
--- lib/capistrano-git-methods/namespace.rb
+++ lib/capistrano-git-methods/namespace.rb
@@ -1,3 +1,5 @@
+require "shell-spinner"
+
 Capistrano::Configuration.instance.load do
   namespace :git do
     set(:branch)  { "master" }
@@ -8,14 +10,18 @@ Capistrano::Configuration.instance.load do
       dirs = [deploy_to, shared_path]
       dirs += shared_children.map { |d| File.join(shared_path, d) }
       run "mkdir -p #{dirs.join(' ')} && chmod g+w #{dirs.join(' ')}"
-      run "git clone #{repository} #{latest_release}"
+      run "git clone #{repository} #{latest_release} --quiet"
     end
 
     desc "Update the deployed code."
     task :update_code, :except => { :no_release => true } do
-      git_cmd = "cd #{latest_release}; git fetch origin; git reset --hard origin/#{fetch(:branch)}"
+      git_cmd = "cd #{latest_release}; git fetch origin --quiet; git reset --hard origin/#{fetch(:branch)} --quiet"
       git_cmd << "; git checkout #{tag}" if tag.to_s.length > 0
-      run git_cmd
+      find_servers({ :role => :web }).each do |server|
+        ShellSpinner "Updating git on #{server}" do
+          run git_cmd, :hosts => [server]
+        end
+      end
     end
 
     desc "Moves the repo back to the previous version of HEAD"
